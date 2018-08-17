d6a133d70cc4446154f85e20f638e6689d5c3419
Kevin Dunn <dunn0407@umn.edu>
2018-08-17 11:01:41 -0500

    fix

--- dev/GURPScharacter.py
+++ dev/GURPScharacter.py
@@ -2,9 +2,11 @@
 import crw
 
 class GURPSCharacter:
-    def __init__(self):
+    def __init__(self, name):
         self.stat = {}
         self.item = {}
+        self.weapon = {}
+        self.armor = {}
         self.advantage = {}
         self.perk = {}
         self.disadvantage = {}
@@ -12,17 +14,20 @@ class GURPSCharacter:
         self.skill = {}
         self.fluff = {}
 
-        #character data init
-        self.setData("data/newStats", self.stat)
-        self.setData("data/newItems", self.item)
-        self.setData("data/newAdvantages", self.advantage)
-        self.setData("data/newPerks", self.perk)
-        self.setData("data/newDisadvantages", self.disadvantage)
-        self.setData("data/newQuirks", self.quirk)
-        self.setData("data/newSkills", self.skill)
-        self.setData("data/newFluff", self.fluff)
+        #character data init - get char data by name folder
+        self.setData("data/characters/" + name + "/Stats", self.stat)
+        self.setData("data/characters/" + name + "/Items", self.item)
+        self.setData("data/characters/" + name + "/Armor", self.armor)
+        self.setData("data/characters/" + name + "/Weapons", self.weapon)
+        self.setData("data/characters/" + name + "/Advantages", self.advantage)
+        self.setData("data/characters/" + name + "/Perks", self.perk)
+        self.setData("data/characters/" + name + "/Disadvantages", self.disadvantage)
+        self.setData("data/characters/" + name + "/Quirks", self.quirk)
+        self.setData("data/characters/" + name + "/Skills", self.skill)
+        self.setData("data/characters/" + name + "/Fluff", self.fluff)
 
+    #read data from csv - name as key, list value as val
     def setData(self, path, dictionary, header = False):
         temp = crw.getData(path)
         for item in temp[not header:]:
-            dictionary[item[0]] = item[1:]
+            dictionary[item[0]] = item[1:]    
