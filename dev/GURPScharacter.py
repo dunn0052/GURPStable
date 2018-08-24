7e273f96104367c31d6582b1311d9c3f931cb299
Kevin Dunn <dunn0407@umn.edu>
2018-08-24 16:04:09 -0500

    save function fixed

--- dev/GURPScharacter.py
+++ dev/GURPScharacter.py
@@ -13,38 +13,50 @@ class GURPSCharacter:
         self.quirk = {}
         self.skill = {}
         self.fluff = {}
-        #temp test points
-        self.point = 125
 
-        
+
     def loadCharacter(self, path = "data/characters/", name = None):
         # character data init - get char data by name folder
-        self.setData(path + name + "/Stats", self.stat)
-        self.setData(path + name + "/Items", self.item)
-        self.setData(path + name + "/Armor", self.armor)
-        self.setData(path + name + "/Weapons", self.weapon)
-        self.setData(path + name + "/Advantages", self.advantage)
-        self.setData(path + name + "/Perks", self.perk)
-        self.setData(path + name + "/Disadvantages", self.disadvantage)
-        self.setData(path + name + "/Quirks", self.quirk)
-        self.setData(path + name + "/Skills", self.skill)
-        self.setData(path + name + "/Fluff", self.fluff)
+        self.loadData(path + name + "/Stats", self.stat)
+        self.loadData(path + name + "/Items", self.item)
+        self.loadData(path + name + "/Armor", self.armor)
+        self.loadData(path + name + "/Weapons", self.weapon)
+        self.loadData(path + name + "/Advantages", self.advantage)
+        self.loadData(path + name + "/Perks", self.perk)
+        self.loadData(path + name + "/Disadvantages", self.disadvantage)
+        self.loadData(path + name + "/Quirks", self.quirk)
+        self.loadData(path + name + "/Skills", self.skill)
+        self.loadData(path + name + "/Fluff", self.fluff)
+
+    def saveCharacter(self, path = "data/characters/saveTest/", name = None):
+        #fix save to load all data
+        crw.setData(path = path, name = "Stats", data = self.formatCharacter(self.stat))
+        return
+
+    def formatCharacter(self, dictionary):
+        n = []
+        for name in dictionary:
+            n.append([name] + dictionary[name])
+        return n
 
     # read data from csv - name as key, list value as val
     # header true => read first row
-    def setData(self, path, dictionary, header = False):
+    def loadData(self, path, dictionary, header = False):
         temp = crw.getData(path)
         for item in temp[not header:]:
-            dictionary[item[0]] = item[1:]    
+            dictionary[item[0]] = item[1:]
 
     # stat2 goes up with stat1 if defined
     def adjSTAT(self, file, stat1, stat2 = None, value = 1, free = False):
         # grabs row data
         tmp1 = crw.findData(file, stat1)
         self.stat[stat1][0] += value
-        #deduct point cost based on value or add if removed
+        # point adjust based on value sign -- dumb speed
         if not free:
-            self.point -= value*int(tmp1[2])
+            if value > 0:
+                self.stat["POINTS"][0] -= int(tmp1[2])
+            else:
+                self.stat["POINTS"][0] += int(tmp1[2])
         if stat2 != None:
             self.stat[stat2][0] += value
         return True # could be used in a button lambda
